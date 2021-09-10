c1b1189e80b0371f1e2d8d059911676dcb46217e
tvsinh <tvsinh.007@gmail.com>
2021-09-10 10:16:30 +0700

    change detailpage

--- src/features/Animal/pages/DetailPage/index.jsx
+++ src/features/Animal/pages/DetailPage/index.jsx
@@ -1,10 +1,23 @@
 import Header from 'components/Header';
-import React from 'react';
+import StorageKeys from 'Constants/storage-keys';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
 import './style.css';
 
 DetailPage.propTypes = {};
 
 function DetailPage(props) {
+  const history = useHistory();
+
+  const isLogin = useSelector((state) => state.auth.current);
+  const logedIn = !!isLogin;
+  useEffect(() => {
+    if (!logedIn) {
+      history.push('/login');
+      localStorage.removeItem(StorageKeys.TOKEN);
+    }
+  }, [history, logedIn]);
   return (
     <>
       <Header />
